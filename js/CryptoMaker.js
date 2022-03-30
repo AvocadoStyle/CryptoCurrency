35b58dfdd4603ba1879b2f139be2cc791e5e3592
AvocadoStyle <edenraf@hotmail.com>
2022-03-30 19:35:21 +0300

    added local storage features to measure the last time checked the values by pressing the button to check the last value we check, compare to the current value that we press right now to check

--- js/CryptoMaker.js
+++ js/CryptoMaker.js
@@ -14,10 +14,43 @@ class CryptoCoin {
 		newBox.innerHTML += "<img src='" + this.imgUrl + "' >";
 		newBox.innerHTML += "<h3>" + this.name + "</h3>";
 		newBox.innerHTML += "<div>Price: " + this.price.toFixed(2) + "</h3>";
+
 		let newBtn = document.createElement("button");
-		newBtn.className = "box";
-		this.parent.appendChild(newBtn);
-		newBtn.innerHTML += "follow coin"
+		// newBtn.className = "box";
+		newBox.appendChild(newBtn)
+		// this.parent.appendChild(newBtn);
+		newBtn.innerHTML += "follow coin and check gap from last time you check"
+		var newLabel = document.createElement("p")
+		newBox.appendChild(newLabel);
+		newBtn.onclick = function(){
+			// newBtn.innerHTML = "<strong>" + this.price.toFixed(2) +
+			//   "</strong>";
+			let oldVal = localStorage.getItem(this.id) //old value
+			let newVal = this.price.toFixed(2); // new value
+			let result = 0;
+			if(localStorage.getItem(this.id) === null) {
+				console.log("first?")
+				newLabel.innerHTML = "first time checking"
+
+			} else {
+				console.log("last?")
+				let result = newVal - oldVal
+				newLabel.innerHTML = "the gap from last time is "
+				if(result > 0){
+					newLabel.innerHTML += "bigger then last time by: "	
+				} else if(result < 0){
+					newLabel.innerHTML += "less then last time by: "	
+				} else {
+					newLabel.innerHTML += "the same as last time: "	
+				}
+				newLabel.innerHTML += result
+				newLabel.innerHTML += ", and before it was: " + oldVal
+
+			}
+			
+			localStorage.setItem(this.id, this.price.toFixed(2))
+			this.isClicked = true
+		}.bind(this)
 		
 
 	}
